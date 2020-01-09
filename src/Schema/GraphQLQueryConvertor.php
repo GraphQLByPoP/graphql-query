16fd54f19cd50f1116d428ed732b15577a55393e
Leo <losoviz@gmail.com>
2020-01-09 14:57:00 +0800

    Convert fields

--- src/Schema/GraphQLQueryConvertor.php
+++ src/Schema/GraphQLQueryConvertor.php
@@ -3,11 +3,13 @@ namespace PoP\GraphQLAPIQuery\Schema;
 
 use Exception;
 use InvalidArgumentException;
+use PoP\FieldQuery\QuerySyntax;
 use Youshido\GraphQL\Parser\Parser;
 use Youshido\GraphQL\Execution\Request;
 use PoP\Translation\TranslationAPIInterface;
 use PoP\ComponentModel\Schema\FeedbackMessageStoreInterface;
 use Youshido\GraphQL\Validator\RequestValidator\RequestValidator;
+use PoP\ComponentModel\Facades\Schema\FieldQueryInterpreterFacade;
 
 class GraphQLQueryConvertor implements GraphQLQueryConvertorInterface
 {
@@ -26,7 +28,7 @@ class GraphQLQueryConvertor implements GraphQLQueryConvertorInterface
     {
         // If the validation throws an error, display it
         try {
-            $parsedData = $this->parseAndCreateRequest($graphQLQuery, $variables);
+            $request = $this->parseAndCreateRequest($graphQLQuery, $variables);
         } catch (Exception $e) {
             // Save the error
             $errorMessage = $e->getMessage();
@@ -34,13 +36,85 @@ class GraphQLQueryConvertor implements GraphQLQueryConvertorInterface
             // Returning nothing will not process the query
             return '';
         }
+        return $this->convertRequestToFieldQuery($request);
+    }
+
+    /**
+     * Convert the GraphQL to its equivalent fieldQuery. The GraphQL syntax is explained in https://graphql.org/learn/queries/
+     *
+     * @param Request $request
+     * @return string
+     */
+    protected function convertRequestToFieldQuery(Request $request): string
+    {
+        $fieldQueryInterpreter = FieldQueryInterpreterFacade::getInstance();
+        $fieldQueries = [];
+        // $debug = [];
+
+        // field
+        foreach ($request->getQueries() as $query) {
+            // $debug[] = print_r($query, true);
+            $fieldQueries[] = $fieldQueryInterpreter->getField(
+                $query->getName(),
+                $query->getFields(),
+                $query->getAlias()
+            );
+        }
+
+        // nested field
+
+        // field arguments
+
+        // aliases
+
+        // fragments
+
+        // operation name
+
+        // variables
+
+        //variables inside fragments
+
+        // default variables
+
+        // directives
+
+        // inline fragments
+
+        // mutations
         // TODO
-        $fieldQuery = sprintf(
-            'echo("%s")@parsedData',
-            print_r($parsedData, true)
+
+        $fieldQuery = implode(
+            QuerySyntax::SYMBOL_QUERYFIELDS_SEPARATOR,
+            $fieldQueries
         );
-        // $fieldQuery = '';
         return $fieldQuery;
+
+        // // Testing
+        // // $debug = $request;
+        // $debug = [
+        //     'getAllOperations()' => $request->getAllOperations(),
+        //     'getQueries()' => $request->getQueries(),
+        //     'getFragments()' => $request->getFragments(),
+        //     // 'getFragment($name)' => $request->getFragment($name),
+        //     'getMutations()' => $request->getMutations(),
+        //     'hasQueries()' => $request->hasQueries(),
+        //     'hasMutations()' => $request->hasMutations(),
+        //     'hasFragments()' => $request->hasFragments(),
+        //     'getVariables()' => $request->getVariables(),
+        //     // 'getVariable($name)' => $request->getVariable($name),
+        //     // 'hasVariable($name)' => $request->hasVariable($name),
+        //     'getQueryVariables()' => $request->getQueryVariables(),
+        //     'getFragmentReferences()' => $request->getFragmentReferences(),
+        //     'getVariableReferences()' => $request->getVariableReferences(),
+        // ];
+
+        // // Temporary code for testing
+        // $fieldQuery = sprintf(
+        //     'echo("%s")@request',
+        //     print_r($debug, true)
+        // );
+        // return $fieldQuery;
     }
 
     /**
@@ -50,7 +124,7 @@ class GraphQLQueryConvertor implements GraphQLQueryConvertorInterface
      * @param array $variables
      * @return void
      */
-    protected function parseAndCreateRequest($payload, $variables = [])
+    protected function parseAndCreateRequest($payload, $variables = []): Request
     {
         if (empty($payload)) {
             throw new InvalidArgumentException($this->translationAPI->__('Must provide an operation.'));
@@ -63,7 +137,7 @@ class GraphQLQueryConvertor implements GraphQLQueryConvertorInterface
         // If the validation fails, it will throw an exception
         (new RequestValidator())->validate($request);
 
-        // Return the parsed data
-        return $parsedData;
+        // Return the request
+        return $request;
     }
 }
