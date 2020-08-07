78bb41a6d32b35b224e914a14be40c6d23b95fae
Leonardo Losoviz <leo@getpop.org>
2020-08-07 15:37:08 +0800

    Moved env variable to API

--- src/ComponentConfiguration.php
+++ src/ComponentConfiguration.php
@@ -12,31 +12,12 @@ class ComponentConfiguration
     use ComponentConfigurationTrait;
 
     private static $enableVariablesAsExpressions;
-    private static $executeQueryBatchInStrictOrder;
 
     public static function enableVariablesAsExpressions(): bool
     {
         // Define properties
         $envVariable = Environment::ENABLE_VARIABLES_AS_EXPRESSIONS;
         $selfProperty = &self::$enableVariablesAsExpressions;
-        $defaultValue = false;
-        $callback = [EnvironmentValueHelpers::class, 'toBool'];
-
-        // Initialize property from the environment/hook
-        self::maybeInitializeConfigurationValue(
-            $envVariable,
-            $selfProperty,
-            $defaultValue,
-            $callback
-        );
-        return $selfProperty;
-    }
-
-    public static function executeQueryBatchInStrictOrder(): bool
-    {
-        // Define properties
-        $envVariable = Environment::EXECUTE_QUERY_BATCH_IN_STRICT_ORDER;
-        $selfProperty = &self::$executeQueryBatchInStrictOrder;
         $defaultValue = false;
         $callback = [EnvironmentValueHelpers::class, 'toBool'];
 
